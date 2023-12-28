0cd0938e88b0989ea34dbb507ed5374f69f47aff
anurodhsingh <anurodh.singh@nagarro.com>
2023-12-28 07:26:20 +0530

    added notification feature

--- forecastingApplication/src/app/home-page/home-page/home-page.component.ts
+++ forecastingApplication/src/app/home-page/home-page/home-page.component.ts
@@ -22,6 +22,7 @@ export class HomePageComponent implements OnInit {
   numericRegex= "^[1-9]+[0-9]*$";
   addLevelErrorsExist: boolean=false;
   createSheetFormErrorsExist: boolean = false;
+  message:string=''
 
   constructor(private formBuilder: FormBuilder,
     private router: Router,
@@ -91,15 +92,38 @@ export class HomePageComponent implements OnInit {
     let creationJsonData=this.getCreationTimeJson();
 
     this.worksheetParametersTransferService.jsonSchemaCreate=creationJsonData;
-    this.forecastManagementService.saveTableSchema(creationJsonData).subscribe((result)=>{
-      console.log("result fetched",result);
-      if(result==-1){
+    this.forecastManagementService.saveTableSchema(creationJsonData).subscribe({
+      next: (result)=>{
+        console.log("result fetched",result);
+        // on success notify
+        this.worksheetParametersTransferService.notify(`Sheet with Name: "${result['tableName']}" has been created successfully!!`); 
+        this.router.navigate(['/worksheet']);
+      },
+      error: (error)=>{
+        console.log(error)
+        console.log(error.error.message)
+        if(error.status == 400){
+        this.message = error.error.message
+        }else{
+          this.message="Something went wrong...please try again later!"
+        }
         this.sheetNameExists=true;
-      } else {
-        this.router.navigate(['/worksheet']); 
+        this.router.navigate(['/']);
       }
-    }); 
-    this.router.navigate(['/worksheet']);
+    })
+    
+    // this.forecastManagementService.saveTableSchema(creationJsonData).subscribe((result)=>{
+    //   console.log("result fetched",result);
+    //   // on success notify
+    //   this.worksheetParametersTransferService.notify(`Sheet with Name: "${result['tableName']}" has been created successfully!!`); 
+    //   this.router.navigate(['/worksheet']);
+    // }, (error)=>{
+    //     console.log(error.error.message)
+    //     this.message = error.error.message
+    //     this.sheetNameExists=true;
+    //     this.router.navigate(['/']);
+    // }); 
+    
   }
 
   getCreationTimeJson(){
